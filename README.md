31bd0b57ce5a8304fbfff6f543204a4bc1e6ea68
Dominik Skórka <Sk0rka.dev@gmail.com>
2023-10-30 15:35:28 +0100

    Update NavBar, created Container

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ npx create-next-app@latest
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
+## Fonts:
+https://fonts.google.com/
--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -1,5 +1,44 @@
+import Link from "next/link";
+import Container from "../Container";
+
+import {Teko} from "next/font/google";
+
+const teko = Teko({subsets: ['latin'], weight: ["500"]})
+
 const NavBar = () => {
-    return ( <div>NavBar</div> );
+    return ( 
+    <div className="
+    sticky
+    top-0
+    w-full
+    bg-slate-200
+    z-30
+    shadow-sm
+    "
+    >
+       
+        <div className="py-4 border-b-[1] ">
+            <Container>
+                <div className="
+                flex
+                items-center
+                justify-between
+                gap-3
+                md:gap-0
+                "> 
+                    <div>
+                        <Link href="/" className={teko.className} style={{ fontSize: '24px' }}>Asnet</Link>
+                        </div>
+                    <div className="hidden md:block">Search</div>
+                    <div className="flex items-center gap-8
+                    md:gap-12">
+                        <div>CartCount</div>
+                        <div>UserMenu</div>
+                    </div>
+                </div>
+            </Container>
+        </div>
+       </div> );
 }
  
 export default NavBar;
