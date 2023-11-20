984149fb020d0d1833dbb49dbc481e4b31ffb80e
Dominik Skórka <Sk0rka.dev@gmail.com>
2023-11-20 14:31:21 +0100

    Fixing whole code (Delate)

--- types/index.ts
+++
@@ -1,7 +0,0 @@
-import { User } from "@prisma/client";
-
-export type SafeUser = Omit <User, "createdAt" | "updatedAt" | "emailVerified"> & {
-    createdAt: string;
-    updateAt: string;
-    emailVerified: string | null;
-};
